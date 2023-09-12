c19ae9d4b7aebf84d298fb8e8b75498315887534
Shaza <shaza.azher13@gmail.com>
2023-09-12 13:07:39 +0500

    Initial commit

---
+++ app/src/main/java/com/SheezaAzher/i191879/page4.java
@@ -0,0 +1,14 @@
+package com.SheezaAzher.i191879;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page4);
+    }
+}
